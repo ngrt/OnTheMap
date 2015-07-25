861991773776e19d4fa812332e2595c5bb60d56a
tarioug <menasra@macbook-pro-de-noufel.home>
2015-07-25 16:28:16 +0200

    adding getSession()

--- OnTheMap/ViewController.swift
+++ OnTheMap/ViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var session: NSURLSession!
+    var sessionID: String? = nil
+    let baseURLSecureString = "https://www.udacity.com/api"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let session = NSURLSession.sharedSession()
 
     }
 
@@ -20,6 +30,67 @@ class ViewController: UIViewController {
 
     }
 
+
+    @IBAction func loginBtn(sender: AnyObject) {
+        getSessionID()
+        
+    }
+    
+    func completeLogin() {
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            let controller = self.storyboard!.instantiateViewControllerWithIdentifier("MapTabBarController") as! UITabBarController
+            self.presentViewController(controller, animated: true, completion: nil)
+        })
+    }
+    
+    func getSessionID() {
+        let methodParameters = [
+        ]
+        
+        let urlString = baseURLSecureString + "/session"
+        let url = NSURL(string: urlString)
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = "{\"udacity\": {\"username\": \"tarioug@gmail.com\", \"password\": \"lsnear99\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let task = session.dataTaskWithRequest(request) {data, response, error in
+            if error != nil {
+                println("Error dans sessionID")
+            } else {
+                let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5))
+                
+                var parsingError: NSError? = nil
+                let parsedResult = NSJSONSerialization.JSONObjectWithData(newData, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
+
+                
+                if let sessionDictionary = parsedResult["session"] as? [String: AnyObject] {
+                    if let sessionID = sessionDictionary["id"] as? String {
+                        self.sessionID = sessionID
+                        self.completeLogin()
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    
+    func escapedParameters(parameters : [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            let stringValue = "\(value)"
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            urlVars += [key + "=" + "\(escapedValue)"]
+        }
+        
+        return (!urlVars.isEmpty ? "?" : "") + join("&", urlVars)
+    }
 
 }
 
